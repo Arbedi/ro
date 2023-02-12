17754dfadc8c779d93d954ebde25f36f70cbe14b
Arbedi <88422462+Arbedi@users.noreply.github.com>
2023-02-12 15:49:42 +0800

    Update ascii.js

--- ascii.js
+++ ascii.js
@@ -17,7 +17,7 @@ class Cell {
     }
     draw(ctx){
         ctx.fillStyle = "white";
-        ctx.fillText(this.symbol, this.x + 0.15, this.y + 0.15);
+		ctx.fillText(this.symbol, this.x + 0.15, this.y + 0.15);
         ctx.fillStyle = this.color;
         ctx.fillText(this.symbol, this.x, this.y);
     }
@@ -94,7 +94,7 @@ function handleSlider(){
         ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
     } else {
         inputLabel.innerHTML = 'Resolution: ' + inputSlider.value + ' px';
-        ctx.font = parseInt(inputSlider.value) * 1.2 + 'px Verdana';
+        ctx.font = parseInt(inputSlider.value) * 1.3 + 'px Verdana';
         effect.draw(parseInt(inputSlider.value));
     }
 }
